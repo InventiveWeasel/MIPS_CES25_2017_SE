d0914b388779f0a5122a125046a8579e74ec7de1
Caio Cesar Saldanha Maia Orejuela Kinelski <mrcaiosaldanha@gmail.com>
2017-06-24 00:27:56 -0300

    Codigo de teste adicionado!

--- codigo_teste_C.c
+++ codigo_teste_C.c
@@ -29,22 +29,23 @@ int main(){
     mem[r[3]+5] = r[0]; // sw
     label3:
     r[4] = mem[r[3]+5]; // lw
-    /*
-    r[5] = r[4] * 2; // mul
+    r[13] = r[13] + 2; // addi
+    r[5] = r[4] * r[13]; // mul
     // nop
     // nop
     r[7] = r[7] + 4; // addi
+    r[14] = r[14] + 5; // addi
+    r[15] = r[15] + 85; // addi
     label4:
-    r[5] = r[5] - 5; // sub
+    r[5] = r[5] - r[14]; // sub
     r[8] = r[8] + 1; // addi
     if (r[8] <= r[7]) goto label4; // ble
-    if (r[5] == 85) goto label5; // beq
+    if (r[5] == r[15]) goto label5; // beq
     goto label6; // jmp
     label5:
     r[31] = r[31] + 1; // addi
     label6:
     r[30] = r[30] + 1; // addi
-    */
     // ================================================
 
     for (i = 0; i < 32; i++)
